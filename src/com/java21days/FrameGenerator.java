a154adb58003902e900dd2430ef1dfc5139eb550
Alejandro Salas <asalas.wd@outlook.com>
2022-11-09 13:42:23 -0500

    Set frame size in constructor

--- src/com/java21days/FrameGenerator.java
+++ src/com/java21days/FrameGenerator.java
@@ -1,4 +1,13 @@
 package com.java21days;
 
+import javax.swing.*;
+import java.util.HashMap;
+
 public class FrameGenerator {
+    public static void main(String[] args) {
+        String title = "Select your option";
+        HashMap<String, String> buttons = new HashMap();
+
+        IconButtonFrame buttonFrame = new IconButtonFrame(title, buttons);
+    }
 }
--- src/com/java21days/IconButtonFrame.java
+++ src/com/java21days/IconButtonFrame.java
@@ -1,12 +1,15 @@
 package com.java21days;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.HashMap;
 
 public class IconButtonFrame extends JFrame {
+    Dimension d = new Dimension(700, 700);
+
     IconButtonFrame(String title, HashMap<String, String> buttons) {
         super(title);
-
+        setSize(d);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
     }
