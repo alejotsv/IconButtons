a20cb9322df7440bf6aaf7ffa0745e0f00f759c5
Alejandro Salas <asalas.wd@outlook.com>
2022-11-09 15:42:39 -0500

    Remove dimension to avoid resizing

--- src/com/java21days/IconButtonFrame.java
+++ src/com/java21days/IconButtonFrame.java
@@ -5,12 +5,10 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class IconButtonFrame extends JFrame {
-    Dimension d = new Dimension(700, 700);
 
     IconButtonFrame(String title, ArrayList<String> buttons) {
         super(title);
         ArrayList<JButton> jButtons = createButtons(buttons);
-        setSize(d);
         JPanel panel = new JPanel();
         for(JButton jButton : jButtons){
             panel.add(jButton);
@@ -19,12 +17,12 @@ public class IconButtonFrame extends JFrame {
         setLocationRelativeTo(null);
         add(panel);
         setVisible(true);
+        pack();
     }
 
     ArrayList<JButton> createButtons(ArrayList<String> buttons) {
         ArrayList<JButton> jButtons = new ArrayList<>();
         for(String button : buttons){
-            System.out.println(button);
             ImageIcon icon = new ImageIcon(getClass().getResource("res/" + button));
             JButton btn = new JButton(icon);
             jButtons.add(btn);
