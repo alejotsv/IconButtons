116d31a6859c6ac9c709eec51d47f4197e53a6aa
Alejandro Salas <asalas.wd@outlook.com>
2022-11-09 14:10:13 -0500

    Add method to create buttons with icons in res folder

--- src/com/java21days/FrameGenerator.java
+++ src/com/java21days/FrameGenerator.java
@@ -1,12 +1,12 @@
 package com.java21days;
 
-import javax.swing.*;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class FrameGenerator {
     public static void main(String[] args) {
         String title = "Select your option";
-        HashMap<String, String> buttons = new HashMap();
+        ArrayList<String> buttons = new ArrayList<>(Arrays.asList("res/download.png", "res/upload.png", "res/delete.png", "res/cancel.png"));
 
         IconButtonFrame buttonFrame = new IconButtonFrame(title, buttons);
     }
--- src/com/java21days/IconButtonFrame.java
+++ src/com/java21days/IconButtonFrame.java
@@ -2,15 +2,27 @@ package com.java21days;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.HashMap;
+import java.util.ArrayList;
 
 public class IconButtonFrame extends JFrame {
     Dimension d = new Dimension(700, 700);
 
-    IconButtonFrame(String title, HashMap<String, String> buttons) {
+    IconButtonFrame(String title, ArrayList<String> buttons) {
         super(title);
+        ArrayList<JButton> jButtons = createButtons(buttons);
         setSize(d);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
         setVisible(true);
+    }
+
+    ArrayList<JButton> createButtons(ArrayList<String> buttons) {
+        ArrayList<JButton> jButtons = new ArrayList<>();
+        for(String button : buttons){
+            ImageIcon icon = new ImageIcon(button);
+            JButton btn = new JButton(icon);
+            jButtons.add(btn);
+        }
+        return jButtons;
     }
 }
